b23f26633c9464ee8a052a203213a697c73d0c56
Jay Miller <kjaymiller@gmail.com>
2019-09-06 07:07:06 -0700

    update frontend

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
-import tempfile
-import bcrypt
+import transcriber
 import boto3
+import maya
 import os
 import responder
 import requests
-import transcriber
+import tempfile
 import logging
+import urllib.parse
 
 logging.basicConfig(level=logging.WARNING)
 # Amazon Information
@@ -13,36 +14,45 @@ bucket= os.environ['BUCKET_NAME']
 storage = boto3.client('s3',)
 transcribe = boto3.client('transcribe')
 
-def check_for_jobs(req, transcribe): 
-	email = req.params.get('email')
-	
-	if email:
-		return transcribe.list_transcription_jobs(JobNameContains=email)
-	
-	
-
+def friendly_date(job):
+    friendly_creation = maya.parse(job['CreationTime']).slang_time()
+    job['CreationTime'] = friendly_creation
+    
+    friendly_completion = maya.parse(job['CompletionTime']).slang_time()
+    job['CompletionTime'] = friendly_completion
+    return job
+    
+def check_for_jobs(email, transcribe=transcribe): 
+    if email:
+        jobs = transcribe.list_transcription_jobs(JobNameContains=email)
+        return map(friendly_date, jobs['TranscriptionJobSummaries'])
+    
 api = responder.API()
+
+def escape_email(req):
+    return req.params.get('email', '').replace('@', 'AT').replace('.', '_')
+
 
 @api.route('/')
 class Index:
 
     def on_get(self, req, resp):
-		jobs = check_for_jobs(transcribe, req)
-	    resp.html = api.template('index.html', jobs=jobs)
+        jobs = check_for_jobs(escape_email(req))
+        logging.debug(jobs)
+        resp.html = api.template('index.html', jobs=jobs)
 
-    async def on_post(self, req, resp,):
+    async def on_post(self, req, resp):
         @api.background.task
-        def upload_file(data, filename, key, email):
+        def upload_file(data, filename, key):
             with tempfile.TemporaryFile() as temp_file:
                 temp_file.write(data['audio_file']['content'])
                 temp_file.seek(0)
                 storage.upload_fileobj(
                         temp_file,
                         Bucket=bucket,
-                        Key=data['email']+key,
+                        Key=key,
                         )
                         
-            email_upload_update()
             transcriber.start_transcription(
                     storage=storage,
                     transcribe=transcribe,
@@ -52,17 +62,24 @@ class Index:
                     lang='en-US',
                     )
 
+        
+        email = escape_email(req)
         data = await req.media(format='files')
         filename = data['audio_file']['filename']
         logging.debug(data['audio_file']['content'])
-        key = data['email'] + transcriber.get_key(filename)
+        key = email + transcriber.get_key(filename)
 
 
         upload_file(data, filename=filename, key=key)
         upload_message = f'{filename} has been uploaded and queued for \
                 transcription'
-		jobs = check_for_jobs(transcribe, req)        	
-        resp.html = api.template('index.html', jobs=jobs, message=upload_message)
+        jobs = check_for_jobs(email)
+        logging.debug(jobs)
+        resp.html = api.template(
+                'index.html',
+                jobs=jobs, 
+                message=upload_message,
+                )
 
 
 api.run()
