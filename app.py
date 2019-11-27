efa3033f9b79cd8a580b794924c994047da27ede
kmiller <kjaymiller@gmail.com>
2019-11-27 10:16:26 -0800

    update

--- app.py
+++ app.py
@@ -84,10 +84,11 @@ def get_transcription_page(key):
     flags = transcriber.flags
     job = transcriber.get_job(key)['TranscriptionJob']
     channels = transcriber.get_transcript(job)['results']['channel_labels']['channels']
-    for channel_label, items in channels:
-        for item in items:
-            item['channel_label'] = channel_label
-    return render_template('transcript.html', flags=flags, job=job, transcription=channels[items],
+    for channel in channels:
+        for item in channel:
+            channel_label = channel['channel_label']
+            item[channel_label] =   channel_label
+    return render_template('transcript.html', flags=flags, job=job, transcription=channels,
     )
 
 
