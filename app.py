f1dd80c2491c8bebdfbeebc4c1b17a75c5cbe55f
kmiller <kjaymiller@gmail.com>
2019-12-03 15:38:03 -0800

    create edit form

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def get_transcription_page(key):
                 {
                     'key': key,
                     'job': job,
-                    'transcription': {datetime.utcnow(): transcriber.get_transcription(job)},
+                    'transcription': {str(datetime.utcnow()): transcriber.get_transcription(job)},
                     })
 
     transcription_text = json_builder.build_transcript(transcript['transcription'])
