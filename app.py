ca75ec44074f8952f5e659f5d70714c4549aadda
kmiller <kjaymiller@gmail.com>
2019-12-03 08:16:31 -0800

    create edit form

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ from flask import (
 
 from forms.forms import SetupForm, UploadForm
 from werkzeug.utils import secure_filename
+from wtforms import FlaskForms
 import wtforms.fields as fields
 import wtforms.validators as validators
 
