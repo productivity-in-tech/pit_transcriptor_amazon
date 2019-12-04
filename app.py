557cd8d396eed0d1e69df12d1a13ddce705cf4f2
kmiller <kjaymiller@gmail.com>
2019-12-04 15:45:06 -0800

    create edit form

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ def get_transcription_page(key):
 
     if len(transcriptions) > 1:
         previous_version = transcriptions[-2][-1]
-        diff = difflib.unified_diff(
+        diff = difflib.HtmlDiff().make_tablel(
                 previous_version.splitlines(keepends=True),
                 transcription_text.splitlines(keepends=True),
                     )
