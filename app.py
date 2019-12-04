8c80e08abdfcefbf13af8224116d5e5e473fc9e4
kmiller <kjaymiller@gmail.com>
2019-12-04 15:00:33 -0800

    create edit form

--- app.py
+++ app.py
@@ -125,7 +125,7 @@ def get_transcription_page(key):
                 'transcript.html',
                 flags=flags,
                 job=job,
-                version_date = arrow.get(version_date, 'YYYYMMDDHHMMss')
+                version_date = arrow.get(version_date, 'YYYYMMDDHHMMss'),
                 form = EditTranscriptionForm(),
                 count = len(re.findall(r'\*.*\*', transcription_text))
         )
