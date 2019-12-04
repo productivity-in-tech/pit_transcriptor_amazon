2d79331a835a992e335cb10120ed1a66c1585dc7
kmiller <kjaymiller@gmail.com>
2019-12-04 15:15:45 -0800

    create edit form

--- app.py
+++ app.py
@@ -114,6 +114,7 @@ def get_transcription_page(key):
 
     if len(transcriptions) > 1:
         previous_version = transcriptions[-2][-1]
+        diff = unified_diff(previous_version, transcription_text)
 
 
     class EditTranscriptionForm(FlaskForm):
