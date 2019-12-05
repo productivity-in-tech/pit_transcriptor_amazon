a3611d2cf8bca5a1431d4164e126fb46a8915ac1
kmiller <kjaymiller@gmail.com>
2019-12-04 16:23:55 -0800

    create edit form

--- app.py
+++ app.py
@@ -115,8 +115,8 @@ def get_transcription_page(key):
     if len(transcriptions) > 1:
         previous_version = transcriptions[-2][-1]
         diff = list(difflib.Differ().compare(
-                previous_version.splitlines(keepends=True),
-                transcription_text.splitlines(keepends=True),
+                previous_version.splitlines(),
+                transcription_text.splitlines(),
                     ))
 
 
@@ -132,7 +132,7 @@ def get_transcription_page(key):
                 version_date = arrow.get(version_date, 'YYYYMMDDHHmmss').format('DD MMM, YYYY HH:ss'),
                 form = EditTranscriptionForm(),
                 count = len(re.findall(r'\*.*\*', transcription_text)),
-                diff = ''.join(diff),
+                diff = '\n'.join(diff),
         )
 
 
