77bf5cf3d112e79d3486feea71c42f8c5972ad43
kjaymiller <kjaymiller@gmail.com>
2020-04-03 16:29:06 -0700

    convert to cli and add bulk_transcriptions

--- audio_splitter.py
+++ audio_splitter.py
@@ -9,87 +9,126 @@ import shutil
 import sys
 import time
 import typing
-
-
-# Pushover Information
-pushover_url = 'https://api.pushover.net/1/messages/.json'
-pushover_data = {
-        'app_token': os.environ['PUSHOVER_APP_TOKEN'],
-        'user_key': os.environ['PUSHOVER_USER_KEY'],
-        }
-
-    return upload
-
-
 def start_transcription(
+        key,
         *,
-        storage=storage,
-        transcribe=transcribe,
-        bucket=bucket,
-        key=key,
-        ChannelIdentification=True
+        storage,
+        transcribe,
+        bucket,
+        ChannelIdentification=True,
         lang='en-US',
         ):
     transcribe_job_uri = f'{storage.meta.endpoint_url}/{bucket}/{key}'
+    click.echo(Path(key).suffix[1:])
     transcribe.start_transcription_job(
             TranscriptionJobName=key,
             Media={'MediaFileUri': transcribe_job_uri},
             MediaFormat= Path(key).suffix[1:],
             LanguageCode=lang,
-            Settings={'ChannelIdentification': True},
+            Settings={'ChannelIdentification': ChannelIdentification},
             )
     return key
 
-
-def _check(key):
-        """Calling check skips the transcription starting point
-and just checks the status for the file"""
-        job = transcribe.get_transcription_job(TranscriptionJobName=key)
-        job_status = job['TranscriptionJob']['TranscriptionJobStatus']
-
-        return job_status
+@click.group()
+def cli():
+    pass
 
 
-
-
-@click.command()
-@click.option('--channels', type=Click.choice([1, 2]), default=1)
+@cli.command()
+@click.option('--channels', is_flag=True)
 @click.option('--check', '-c', is_flag=True)
 @click.argument('base_audio_file')
-def _main(
+def transcription(
         channels,
         base_audio_file,
-        skip_upload,
         check,
+        directory,
         ):
 
     # Amazon Information
-    bucket= os.environ['BUCKET_NAME']
+    bucket = os.environ['BUCKET_NAME']
     storage = boto3.client('s3')
     transcribe = boto3.client('transcribe')
     key=Path(base_audio_file).name.replace(' ', '')
 
-   if check:
-       if _check(key) != 'IN_PROGRESS':
+    if check:
+        job = transcribe.get_transcription_job(TranscriptionJobName=key)
+        job_status = job['TranscriptionJob']['TranscriptionJobStatus']
+
+        if job_status != 'IN_PROGRESS':
             job_uri = job['TranscriptionJob']['Transcript']['TranscriptFileUri']
             r = requests.get(job_uri)
             r.raise_for_status()
 
             with open(f'{key}.json', 'w') as json_file:
                 json_file.write(json.dumps(r.json()))
-            print('Job: {key} complete and written')
+
+            click.echo('Job: {key} complete and written')
 
         return # Terminates the script
 
+
     storage.upload_file(
-                Filename=base_audio_file,
+            Filename=base_audio_file,
+            Bucket=bucket,
+            Key=key,
+            )
+
+    return start_transcription(
+        key=key, bucket=bucket, storage=storage,
+        transcribe=transcribe, ChannelIdentification=channels,
+        )
+
+
+
+@cli.command()
+@click.argument('directory')
+@click.option('--check', '-c', is_flag=True)
+@click.option('--channels', is_flag=True)
+@click.option('--file-format', default='mp3')
+def bulk_transcription(
+        directory,
+        check,
+        channels,
+        file_format,
+        ):
+
+    bucket = os.environ['BUCKET_NAME']
+    storage = boto3.client('s3')
+    transcribe = boto3.client('transcribe')
+
+
+    for path in Path(directory).glob(f'*.{file_format}'):
+        key = path.name.replace(' ', '')
+
+        if check:
+            job = transcribe.get_transcription_job(TranscriptionJobName=key)
+            job_status = job['TranscriptionJob']['TranscriptionJobStatus']
+
+            if job_status != 'IN_PROGRESS':
+                job_uri = job['TranscriptionJob']['Transcript']['TranscriptFileUri']
+                r = requests.get(job_uri)
+                r.raise_for_status()
+
+                with open(f'{key}.json', 'w') as json_file:
+                    json_file.write(json.dumps(r.json()))
+
+                click.echo(f'Job: {key} complete and written')
+
+            return # Terminates the script
+
+        storage.upload_file(
+                Filename=str(path),
                 Bucket=bucket,
                 Key=key,
                 )
 
-    channel_count = {1: False, 2: True}
-    return start_transcription(ChannelIdentification=channel_count[channels])
+        start_transcription(
+            key=key, bucket=bucket, storage=storage,
+            transcribe=transcribe, ChannelIdentification=channels,
+            )
 
+    return
 
 if __name__ == '__main__':
-    _main()
+    cli()
